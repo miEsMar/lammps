6c604d8fa0ce88768806866a972f42b7069bb46c
miEsMar <michele.espositomarzino@gmail.com>
2025-01-08 13:50:54 +0100

    [NEW] Add make shell script

---
+++ make.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+module purge
+module load gcc
+module load cmake
+
+if false; then
+    module load openmpi/4.1.5-gcc
+else
+    export PATH=~/ompi/install/lib:$PATH
+    export PATH=~/ompi/install/bin:$PATH
+fi
+
+build_dir="$( pwd )/build"
+if [ ! -d "${build_dir}" ]; then mkdir "${build_dir}"; fi
+
+install_dir="$( pwd )/install"
+
+cmake \
+   -D CMAKE_INSTALL_PREFIX=${install_dir} \
+   -D CMAKE_C_COMPILER=gcc \
+   -D CMAKE_CXX_COMPILER=g++ \
+   -D CMAKE_Fortran_COMPILER=gfortran \
+   -D CMAKE_BUILD_TYPE=Debug \
+   -D BUILD_MPI=yes \
+   -D BUILD_OMP=yes \
+   -D BUILD_SHARED_LIBS=yes \
+   -D LAMMPS_MACHINE=mpi \
+   -S ./cmake -B ${build_dir} $*
+
+cmake --build ${build_dir} -j
+
